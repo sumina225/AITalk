0c78a9d17bc7b47e1cdb6f8aec5f59984dea5f73
kkm <kkm971007@naver.com>
2025-02-03 12:33:24 +0900

    :wrench: [Chore] Spring Boot 프로젝트 초기 설정 jira : S12P11E102-191

---
+++ AITalk/src/test/java/com/ssafy/aitalk/AiTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.aitalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

af9acca884b3cbe939c0eeb0a2b0c9ec265b9584
김우영 <skajtwlkt@naver.com>
2025-02-04 17:32:48 +0900

    :art [style] CardWordPage 충돌 해결위해 기본 function 틀 수정

--- Fe/vite-project/src/pages/CardWordPage.tsx
+++ Fe/vite-project/src/pages/CardWordPage.tsx
@@ -1,9 +1,3 @@
-import Images from '../components/Images/DetailImages';
-
 export default function CardWordPage() {
-  return (
-    <div>
-      <Images width={300} height={700} />
-    </div>
-  );
+  return <div></div>;
 }

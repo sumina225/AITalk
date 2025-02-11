daab60efe23cb7581b7b0408d5c5cfc0f228816f
kkm <kkm971007@naver.com>
2025-02-11 20:46:52 +0900

    :twisted_rightwards_arrows: [merge] 유저기능 브랜치 병합

--- Be/AITalk/src/main/java/com/ssafy/aitalk/user/mapper/UserMapper.java
+++ Be/AITalk/src/main/java/com/ssafy/aitalk/user/mapper/UserMapper.java
@@ -13,4 +13,22 @@ public interface UserMapper {
     int countById(@Param("id") String id);  // 아이디 중복 확인
 
     int countByEmail(@Param("email") String email);  // 이메일 중복 확인
+
+    User findInfoById(@Param("id") int id); // name으로 로그인한 사용자 찾기
+
+    void updateUserInfo(@Param("id") int id,
+                        @Param("email") String email,
+                        @Param("phoneNumber") String phoneNumber);
+
+    void deleteUser(@Param("id") int id);
+
+    String findIdByEmail(@Param("email") String email);  // 아이디 찾기
+
+
+    String findEmailById(@Param("id") String id);
+
+//    @Update("UPDATE speech_therapist SET password = #{password} WHERE id = #{id}")
+    int updatePasswordById(@Param("id") String id, @Param("password") String password);
+
+
 }

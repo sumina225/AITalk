64b7db2e3650ee354d1351a8f7dca68e064fa76f
천세윤 <128568993+yooniverse7@users.noreply.github.com>
2025-02-12 01:48:58 +0900

    :sparkles: [feat] React Schedule 페이지 작성하기 jira : S12P11E102-395

--- Fe/admin-project/src/components/main/schedule/DayScheduleComponent.tsx
+++ Fe/admin-project/src/components/main/schedule/DayScheduleComponent.tsx
@@ -1,3 +1,31 @@
-export default function DayScheduleComponent() {
-  return <div>DayScheduleComponent</div>;
+interface DayScheduleProps {
+  date: Date;
+  events: { id: string; title: string; start: string }[];
+  onClose: () => void;
 }
+
+const DayScheduleComponent = ({ date, events, onClose }: DayScheduleProps) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <h2>{date.toDateString()} 일정 목록</h2>
+
+        {events.length > 0 ? (
+          <ul>
+            {events.map((event) => (
+              <li key={event.id} className="event-item">
+                {event.title}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>일정이 없습니다.</p>
+        )}
+
+        <button onClick={onClose}>닫기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DayScheduleComponent;

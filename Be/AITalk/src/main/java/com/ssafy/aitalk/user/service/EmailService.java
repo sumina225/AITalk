d638ec6fd76338ad0f354358ab6092e21462b6cb
이수환 <qkqhzz120@naver.com>
2025-02-12 00:57:31 +0900

    :sparkles: [feat] 이미지생성

--- Be/AITalk/src/main/java/com/ssafy/aitalk/user/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.aitalk.user.service;
-
-import jakarta.mail.MessagingException;
-
-public interface EmailService {
-    void sendEmail(String to, String subject, String text) throws MessagingException;
-}

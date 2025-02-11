3b437a464dc986b5e97e7fa3720b7e7f56d6ec49
박수민 <psm5927@naver.com>
2025-02-11 14:31:19 +0900

    Reapply ":twisted_rightwards_arrows: [merge] feature/be/sentence -> develop"
    This reverts commit a4af5080fcea12c5cca6e5baa2568e03e3599aab.

--- Fe/vite-project/src/components/Texts/PlaySelectText.tsx
+++
@@ -1,5 +0,0 @@
-import './PlaySelectText.css';
-
-export default function PlaySelectText() {
-  return <div className="PlaySelectText">🐻 오늘은 무엇을 하고 놀까요? 🎈</div>;
-}
--- LocalBE/AITalkFlask/app/routes/therapist_login.py
+++ LocalBE/AITalkFlask/app/routes/therapist_login.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.services.therapist_login_service import get_card_info_after_tagging
+from app.services.login_service import login_user
 
 login_bp = Blueprint('login_bp', __name__)
 
@@ -8,3 +9,18 @@ login_bp = Blueprint('login_bp', __name__)
 def read_nfc_and_get_card():
     therapist_info, status_code = get_card_info_after_tagging()
     return jsonify(therapist_info), status_code
+
+@login_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user_id = data.get('id')
+    password = data.get('password')
+
+    if not user_id or not password:
+        return jsonify({'message': 'ID와 비밀번호를 입력하세요.'}), 400
+
+    user = login_user(user_id, password)
+    if user:
+        return jsonify({'message': '로그인 성공', 'therapist_id': user.therapist_id}), 200
+    else:
+        return jsonify({'message': 'ID 또는 비밀번호가 올바르지 않습니다.'}), 401

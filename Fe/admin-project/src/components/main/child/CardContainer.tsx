ba7e969a4611552e43acc7094527c2aaf88ceb4c
천세윤 <tpdbs0701@gmail.com>
2025-02-11 14:37:01 +0900

    :sparkles: [feat] React care child list 페이지 작성하기 jira : S12P11E102-397

--- Fe/admin-project/src/components/main/child/CardContainer.tsx
+++ Fe/admin-project/src/components/main/child/CardContainer.tsx
@@ -1,3 +1,24 @@
-export default function CardContainer() {
-  return <div>CardContainer</div>;
+import './CardContainer.css';
+import profile from '../../../assets/images/main/profile_img.png';
+
+interface CardContainerProps {
+  childName: string;
+  age: number;
+  disabilityType: string;
+  center: number; 
+}
+
+export default function CardContainer({ childName, age, disabilityType, center }: CardContainerProps) {
+  return (
+    <div className="card-container">
+      <img src={profile} alt='profile image' />
+      <div className="card-text-container">
+        <h2>{childName}</h2>
+        <span>{age}세 ({age * 12}개월)</span>
+        <hr />
+        <p>진료 사항: {disabilityType}</p>
+        <p>센터: {center}</p>
+      </div>
+    </div>
+  );
 }
--- Fe/admin-project/src/pages/main/child/ChildListPage.tsx
+++ Fe/admin-project/src/pages/main/child/ChildListPage.tsx
@@ -1,3 +1,22 @@
+import './ChildListPage.css';
+import { careChildrenDummyData } from '../../../utils/careChildrenDummyData';
+import CardContainer from '../../../components/main/child/CardContainer';
+
 export default function ChildListPage() {
-  return <div>ChildListPage</div>;
+  return (
+    <div className="child-list-container">
+      <h1>치료 아동 관리</h1>
+      <div className="card-grid">
+        {careChildrenDummyData.map((child) => (
+          <CardContainer
+            key={child.id}
+            childName={child.childName}
+            age={child.age}
+            disabilityType={child.disabilityType}
+            center={child.centerId} 
+          />
+        ))}
+      </div>
+    </div>
+  );
 }

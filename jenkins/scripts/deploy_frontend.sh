f7c2a428512479282677a41bb57238bcc603ff4d
이수환 <qkqhzz120@naver.com>
2025-02-13 15:35:20 +0900

    :arrows_counterclockwise: [build] front Dockerfile 포트수정

--- jenkins/scripts/deploy_frontend.sh
+++ jenkins/scripts/deploy_frontend.sh
@@ -8,8 +8,8 @@ docker pull suhwany/aitalk:frontend-latest
 docker stop manage-children-front || true
 docker rm manage-children-front || true
 
-# 새 컨테이너 실행
-docker run -d --name manage-children -p 3000:80 suhwany/aitalk:frontend-latest
+# 새 컨테이너 실행 (3000번 포트에서 실행)
+docker run -d --name manage-children-front -p 3000:3000 suhwany/aitalk:frontend-latest
 
 # 사용하지 않는 Docker 이미지 정리
 docker image prune -a -f

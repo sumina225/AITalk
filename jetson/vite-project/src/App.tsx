80bf209c9ca3bc698617a4db0d55b19ec1e54952
김우영 <skajtwlkt@naver.com>
2025-02-13 19:09:50 +0900

    :sparkles: [feat] CameraPlaySelectThreeSentencePage 구성하기(날라간 페이지 재생성 하는 중)

--- jetson/vite-project/src/App.tsx
+++ jetson/vite-project/src/App.tsx
@@ -9,6 +9,7 @@ import CardPlaySelectPage from './pages/CardPlaySelectPage';
 import CardPlaySelectWordPage from './pages/CardPlaySelectWordPage';
 import CardPlaySelectWordVerbPage from './pages/CardPlaySelectWordVerbPage';
 import CardPlaySelectWordVerbSentencePage from './pages/CardPlaySelectWordVerbSentencePage';
+import CardPlaySelectThreeSentencePage from './pages/CardPlaySelectThreeSentencePage';
 
 import CameraScanPage from './pages/CameraScanPage';
 import CameraImageGeneratePage from './pages/CameraImageGeneratePage';
@@ -16,6 +17,7 @@ import CameraPlaySelectPage from './pages/CameraPlaySelectPage';
 import CameraPlaySelectWordPage from './pages/CameraPlaySelectWordPage';
 import CameraPlaySelectWordVerbPage from './pages/CameraPlaySelectWordVerbPage';
 import CameraPlaySelectWordVerbSentencePage from './pages/CameraPlaySelectWordVerbSentencePage';
+import CameraPlaySelectThreeSentencePage from './pages/CameraPlaySelectThreeSentencePage';
 
 import AiTalkPage from './pages/AiTalkPage';
 
@@ -26,7 +28,6 @@ import TherapistFaceResisterCompletePage from './pages/TherapistFaceRegisterComp
 import HomeAfterLoginPage from './pages/HomeAfterLoginPage';
 import KidSelectPage from './pages/KidSelectPage';
 import KidFaceLoginPage from './pages/KidFaceLoginPage';
-import CardPlaySelectThreeSentencePage from './pages/CardPlaySelectThreeSentencePage';
 
 export default function App() {
   return (
@@ -93,6 +94,10 @@ function MainRoutes() {
       <Route
         path="/camera-play-select/word/verb/sentence"
         element={<CameraPlaySelectWordVerbSentencePage />}
+      ></Route>
+      <Route
+        path="/camera-play-select/three-sentence"
+        element={<CameraPlaySelectThreeSentencePage />}
       ></Route>
       <Route path="/ai-talk" element={<AiTalkPage />} />
       <Route

ca824190e084addbebef862c72e983b3ea2a4072
최진문 <rlawjsdlf13@naver.com>
2025-02-13 14:21:40 +0900

    :recycle: [Refactor] merge 이후 변경점 수정하여 동작 확인(card 태그 로그인, childList 페이지 api 연결 확인

--- Fe/vite-project/src/hooks/UseFetch.tsx
+++ Fe/vite-project/src/hooks/UseFetch.tsx
@@ -44,7 +44,6 @@ export function UseFetch<T>(url: string): State<T> {
   const therapist_id = useSelector((state: RootState) => state.user.currentUser?.therapist_id);
 
   useEffect(() => {
-    console.log(therapist_id)
     if (therapist_id === undefined || therapist_id === null) {
       console.log('아이디 어딨니!')
     }
@@ -56,7 +55,7 @@ export function UseFetch<T>(url: string): State<T> {
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ therapist_id: therapist_id }), // Redux Persist에 저장된 therapist_id를 요청 본문에 포함
+          body: JSON.stringify({ therapistId: therapist_id }), // Redux Persist에 저장된 therapist_id를 요청 본문에 포함
         });
 
         if (!response.ok)

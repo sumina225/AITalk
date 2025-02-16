b470ef34663f32c65fcc912ab0f5c3028e95db91
jinmoon23 <wlsansdlek1@me.com>
2025-02-16 20:31:57 +0900

    :recycle: [Refactor] feature/fe/final_1 to develop merge

---
+++ Fe/admin-project/src/pages/user/UserFindPwPage.tsx
@@ -0,0 +1,52 @@
+import './UserSignUpPage.css';
+import LogoSVG from '../../assets/User/AiTalkLogo.svg';
+import { useFindPw } from '../../hooks/user/useFindPw';
+import { InputField } from '../../components/user/common/InputComponent';
+import EmailAuthComponent from '../../components/user/common/EmailAuthComponent';
+import { useEmailVerify } from '../../hooks/user/useEmailAuth';
+import { useEmailAuthConfirm } from '../../hooks/user/useEmailAuthConfirm';
+
+const UserFindPwPage: React.FC = () => {
+  const {
+    name,
+    setName,
+    id,
+    setId,
+    email,
+    setEmail,
+    errorMessage,
+  } = useFindPw();
+  const { confirmEmail } = useEmailAuthConfirm();
+
+  const { handleEmailVerify } = useEmailVerify();
+
+  return (
+    <div className="user-signup-container">
+      <div className="signup-box">
+        <img src={LogoSVG} alt="logoImage" className="logo-image" />
+        <h1>비밀번호 찾기</h1>
+        <InputField
+          type="text"
+          placeholder="이름"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <InputField
+          type="text"
+          placeholder="ID"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <EmailAuthComponent
+          email={email}
+          onEmailChange={(e) => setEmail(e.target.value)}
+          onVerify={handleEmailVerify}
+          onConfirm={confirmEmail}
+          from='findPw'
+        />
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </div>
+    </div>
+  );
+};
+export default UserFindPwPage;

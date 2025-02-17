6f7559d2318a603951e16f8683b588a2febb0791
이수환 <qkqhzz120@naver.com>
2025-02-17 11:37:43 +0900

    :arrows_counterclockwise: [build] host 추가

--- jenkins/scripts/deploy_frontend.sh
+++ jenkins/scripts/deploy_frontend.sh
@@ -11,8 +11,8 @@ docker rm manage-children-front || true
 # 실행 권한 문제 해결
 chmod +x node_modules/.bin/tsc || true
 
-# 새 컨테이너 실행 (Vite preview 모드)
-docker run -d --name manage-children-front -p 4173:4173 suhwany/aitalk:frontend-latest
+# 새 컨테이너 실행 (Vite preview 모드, --host 0.0.0.0 추가)
+docker run -d --name manage-children-front -p 4173:4173 suhwany/aitalk:frontend-latest npm run preview -- --host 0.0.0.0
 
 # 사용하지 않는 Docker 이미지 정리
 docker image prune -a -f

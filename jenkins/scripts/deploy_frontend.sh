69e3c36a5b84726e823caf4646936245709b424c
이수환 <qkqhzz120@naver.com>
2025-02-17 21:49:59 +0900

    :arrows_counterclockwise: [build] docker network 연결 확인 로그 추가

--- jenkins/scripts/deploy_frontend.sh
+++ jenkins/scripts/deploy_frontend.sh
@@ -8,12 +8,16 @@ docker pull suhwany/aitalk:frontend-latest
 docker stop manage-children-front || true
 docker rm manage-children-front || true
 
-# ✅ 새 컨테이너 실행 (Nginx 사용)
+# ✅ 네트워크 연결 확인
+docker network ls | grep my_network || docker network create my_network
+
+# ✅ 프론트 컨테이너 실행 (Nginx 사용)
 docker run -d --name manage-children-front \
   --network my_network \
   -p 80:80 -p 443:443 \
   -v /etc/letsencrypt:/etc/letsencrypt:ro \
   suhwany/aitalk:frontend-latest
+
 # 사용하지 않는 Docker 이미지 정리
 docker image prune -a -f
 

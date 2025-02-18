b804aa67b32b564fc747db67d181384bd44799b4
이수환 <qkqhzz120@naver.com>
2025-02-18 15:45:18 +0900

    :wrench: [chore] baseURL 수정

--- Fe/admin-project/src/utils/axiosInstance.ts
+++ Fe/admin-project/src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://i12e102.p.ssafy.io/api',
   headers: {
     'Content-Type': 'application/json',
   },

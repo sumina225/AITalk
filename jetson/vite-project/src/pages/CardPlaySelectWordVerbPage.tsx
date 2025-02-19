10da3805191ccfd56cd5ae3f80f526eb21d9a3af
김우영 <skajtwlkt@naver.com>
2025-02-20 00:47:29 +0900

    :art: [style] CardPlaySelectWordVerbPage 수정중

--- jetson/vite-project/src/pages/CardPlaySelectWordVerbPage.tsx
+++ jetson/vite-project/src/pages/CardPlaySelectWordVerbPage.tsx
@@ -36,7 +36,7 @@ export default function CardPlaySelectWordVerbPage() {
       navigate('/card-play-select/word/verb/sentence', {
         state: { firstCard, secondCard },
       });
-    }, 7000);
+    }, 700000);
 
     return () => clearTimeout(timer);
   }, [navigate, firstCard, secondCard]);

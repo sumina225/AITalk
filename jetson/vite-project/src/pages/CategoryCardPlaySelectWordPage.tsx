c5d0fddae33a6e35e8611f0c7c04a4c0da1d93af
김우영 <skajtwlkt@naver.com>
2025-02-20 12:27:26 +0900

    :art: [style] 범주화 CardPlay 관련 페이지 css 모두 완료

--- jetson/vite-project/src/pages/CategoryCardPlaySelectWordPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function CategoryCardPlaySelectWordPage() {
-  return <div>CategoryCardPlaySelectWordPage</div>;
-}
--- jetson/vite-project/src/pages/CategoryCardPlaySelectWordVerbPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function CategoryCardPlaySelectWordVerbPage() {
-  return <div>CategoryCardPlaySelectWordVerbPage</div>;
-}
